383fba053f81fc0ef96bb3c6333238111a95dc93
Siraj Berry <siraj.berry@idento.fr>
2022-07-05 15:15:30 +0200

    commit comments

--- server.js
+++ server.js
@@ -1,42 +1,4 @@
-// const express = require('express');
-// const app = express();
-
-// app.use(requireHTTPS);
-// app.use(express.static('./dist/my-app'));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/my-app/'}),
-// );
-
-// app.listen(8088, function () {
-//     console.log("Application listening at http://localhost:8088")
-// })
-
-
-// function requireHTTPS(req, res, next) {
-//     // The 'x-forwarded-proto' check is for Heroku
-//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//         return res.redirect('https://' + req.get('host') + req.url);
-//     }
-//     next();
-// }
-
-// const express = require('express');
-// const app = express();
-// app.use(express.static(__dirname+ '\\'+'dist'+ '\\'+'my-app'));
-// console.log(__dirname+ '\\'+'dist'+ '\\'+'my-app')
-// app.get('/*', (req, res) =>
-// {
-//     res.sendFile(__dirname+ '\\'+ 'dist' +'\\'+'my-app '+'\\' +' index.html');
-// }
-// );
-
-// app.listen(8089, function () {
-// })
-
-
-
-//Install express server
+/* README: this file runs everytime we push to the i2fl repository*/
 const express = require('express');
 const path = require('path');
 
@@ -49,6 +11,10 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8089);
+
+
+/* 
+References: https://itnext.io/how-to-deploy-angular-application-to-heroku-1d56e09c5147
+command: git push --set-upstream origin master
+*/
