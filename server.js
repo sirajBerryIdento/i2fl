890db087773228004ccb1fa29bb98ec718d24695
Siraj Berry <siraj.berry@idento.fr>
2022-07-05 14:07:54 +0200

    fixing path to dist folder

--- server.js
+++ server.js
@@ -24,14 +24,14 @@
 const express = require('express');
 const app = express();
 // app.use(express.static(__dirname+'/dist/my-app'));
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/my-app'));
 app.get('/*', (req, res) =>
 {
     // res.sendFile(__dirname+'/dist/my-app/index.html');
-    res.sendFile('index.html', { root: 'dist/' });
+    res.sendFile('index.html', { root: 'dist/my-app/' });
 }
 );
 
 app.listen(8088, function () {
-    console.log("Application listening at http://localhost:8088")
+    console.log("Application listening at http://localhost:8088   __dirname: ", __dirname)
 })
